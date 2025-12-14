f1102bb9fd31a3d79a5e52ec43fed940233d41c9
Timur Bohach <73005@student.wsiz.edu.pl>
2025-12-14 21:29:18 +0100

    lab5

---
+++ lab5/zad1.py
@@ -0,0 +1,21 @@
+import random
+
+szczesliwy_num = random.randint(1, 30)
+print("Szczęśliwy numerek:", szczesliwy_num)
+
+
+roczniki = [2005, 2006, 2004, 2005, 2007, 2006]
+szczesliwy_rocznik = random.choice(roczniki)
+
+print("Szczęśliwy rocznik:", szczesliwy_rocznik)
+
+
+
+
+liczby = list(range(1, 50))
+losowanie = random.sample(liczby, 6)
+
+print("Wylosowane liczby:", losowanie)
+
+
+
